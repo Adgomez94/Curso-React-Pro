64c4f96493c2ac360923c129b4c2861c30312ef5
Adrian Duran <duranadria77@gmail.com>
2022-05-30 11:21:48 +0100

    Control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { onChangeArgs, Product } from '../interfaces/interfaces'
 
 interface useProductArgs {
@@ -10,9 +10,6 @@ interface useProductArgs {
 const useProduct = ({ onChange, product, value = 0 }:useProductArgs) => {
   const [counter, setCounter] = useState(value)
 
-  // este valor nos indica que si el padre es quien controla los componentes o no
-  // eso quiere decir que si hay una sincronizacion que tanto el padre y el hijo cambian
-  const isControlled =useRef(!!onChange)
 
   useEffect(() => {
     setCounter(value)
@@ -20,11 +17,6 @@ const useProduct = ({ onChange, product, value = 0 }:useProductArgs) => {
   
   const increase = (prev: number) => {
 
-    if(isControlled.current) {
-      // esto no niega lo que esta haciendo es indicar que este valor no es undefine porque se comprobo
-      // es como hacer if(isControlled.current && onChange)
-      return onChange!({ counter:prev, product }) 
-    }
      
     const newValue = Math.max(counter + prev, 0)
     console.log(newValue)
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -8,35 +8,19 @@ export const useShoppingCart = () => {
   
   const onProductCountChange = ({ counter,product }:{product: Product, counter:number}) => {
 
-    setShoppingCart( shoppingCart =>{
-      const productInCart: ProductInCart = shoppingCart[product.id] || { ...product, counter: 0 }
-
-      if( Math.max( productInCart.counter + counter, 0 ) > 0) {
-        productInCart.counter += counter
-        return {
-          ...shoppingCart,
-          [product.id]: productInCart
-        }
-      }
-
-      // Borrar producto
-      const { [product.id]: deleteProduct, ...restShoppingCart  } = shoppingCart
-      return restShoppingCart
-
-    })
     
     // Forma que mas le gusta a fernando
-    // setShoppingCart( shoppingCart =>{
-    //   if(!counter) {
-    //     const { [product.id]: deleteProduct, ...restShoppingCart  } = shoppingCart
-    //     return restShoppingCart
-    //   } 
+    setShoppingCart( shoppingCart =>{
+      if(!counter) {
+        const { [product.id]: deleteProduct, ...restShoppingCart  } = shoppingCart
+        return restShoppingCart
+      } 
 
-    //   return {
-    //     ...shoppingCart,
-    //     [product.id]: {...product, counter}
-    //   }
-    // })
+      return {
+        ...shoppingCart,
+        [product.id]: {...product, counter}
+      }
+    })
   }
 
   return {
